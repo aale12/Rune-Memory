7fbeea0c3eca59109c9468859562836770c9b36f
winkie224 <leanthony104@yahoo.com>
2019-01-08 23:45:36 -0600

    Updates

---
+++ precache-manifest.d7ca9477a26e1d15820a36758c6f199f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd08aa7521ee2c0921c",
+    "url": "/aale12/Rune-Memory/static/js/runtime~main.cfd08aa7.js"
+  },
+  {
+    "revision": "f75303e6edd53ce985f3",
+    "url": "/aale12/Rune-Memory/static/js/main.f75303e6.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/aale12/Rune-Memory/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "f75303e6edd53ce985f3",
+    "url": "/aale12/Rune-Memory/static/css/main.4fea9b5b.chunk.css"
+  },
+  {
+    "revision": "659e7ecdfa3d0e348caa7ab918f8b4f2",
+    "url": "/aale12/Rune-Memory/index.html"
+  }
+];
